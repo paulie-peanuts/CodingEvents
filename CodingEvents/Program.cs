a3139cfe44566c3893b39c41d7df9c594afd1471
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-06-12 12:16:45 -0500

    login changed

--- CodingEvents/Program.cs
+++ CodingEvents/Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var connectionString = "server=localhost;user=test-user;password=test-user;database=coding-events";
-var serverVersion = new MySqlServerVersion(new Version(8, 0,29));
+var connectionString = "server=localhost;user=codingevents;password=codingevents123;database=coding-events";
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 36));
 
 builder.Services.AddDbContext<EventDbContext>(dbContextOptions => dbContextOptions.UseMySql(connectionString, serverVersion));
 
